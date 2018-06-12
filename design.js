1f46210c2d9c2a8f82a29a8eeb2b2e9787053dce
Ybira <koszotam@gmail.com>
2018-06-12 20:56:25 +0200

    save/load

--- design.js
+++ design.js
@@ -1,3 +1,5 @@
+const {table} = require('table');
+
 const logo = () => {
   console.log('▄▀▀█▄▄   ▄▀▀▄ ▄▀▀▄  ▄▀▀▄ ▀▄  ▄▀▀▀▀▄   ▄▀▀█▄▄▄▄  ▄▀▀▀▀▄   ▄▀▀▄ ▀▄  ▄▀▀▀▀▄            ');
   console.log('█ ▄▀   █ █   █    █ █  █ █ █ █        ▐  ▄▀   ▐ █      █ █  █ █ █ █ █   ▐            ');
@@ -22,6 +24,20 @@ const logo = () => {
   console.log('   ▐                 ▐        ▐       ▐         ▐                                    ');
 };
 
+const sheetDesign = (charSheet) => {
+  let designTable = [
+    ['Name:', charSheet.name, 'Race:', charSheet.race],
+    ['Strength (Str):', charSheet.attributes.Str, 'Str Modifier:', charSheet.modifiers.StrMOD],
+    ['Dexterity (Dex):', charSheet.attributes.Dex, 'Dex Modifier:', charSheet.modifiers.DexMOD],
+    ['Constitution (Con):', charSheet.attributes.Con, 'Con Modifier:', charSheet.modifiers.ConMOD],
+    ['Intelligence (Int):', charSheet.attributes.Int, 'Int Modifier:', charSheet.modifiers.IntMOD],
+    ['Wisdom (Wis):', charSheet.attributes.Wis, 'Wis Modifier:', charSheet.modifiers.WisMOD],
+    ['Charisma (Cha):', charSheet.attributes.Cha, 'Cha Modifier:', charSheet.modifiers.ChaMOD]
+  ];
+  console.log(table(designTable));
+};
+
 module.exports = {
-  logo
+  logo,
+  sheetDesign
 };
--- functions/functions.js
+++ functions/functions.js
@@ -1,19 +1,57 @@
 const readlineSync = require('readline-sync');
+const dice = require('./dice');
+const design = require('../design');
+const constants = require('../constants');
+const clear = require('console-clear');
+const fs = require('fs');
 
 const mainMenu = () => {
   let answers = ['Create Character', 'Load Character', 'Quit Game'];
   let index = readlineSync.keyInSelect(answers, '');
+
   return index;
 };
 
-const newCharacter = (charSheet) => {
-  charSheet.name = readlineSync.question('Character Name: ');
+const createCharacter = () => {
+  let charSheet = constants.blankCharacter;
+
+  clear();
+  design.sheetDesign(charSheet);
+  charSheet = chooseName(charSheet);
+
+  clear();
+  design.sheetDesign(charSheet);
+  charSheet = chooseRace(charSheet);
+  charSheet = chooseAttributes(charSheet);
+
+  saveChars(charSheet);
+
+  return charSheet;
+};
+
+const saveChars = (charSheet) => {
+  fs.writeFileSync(`saved/${charSheet.name}.json`, JSON.stringify(charSheet));
+};
+
+const loadChars = () => {
+  const savedFolder = './saved/';
+  let files = [];
+  fs.readdirSync(savedFolder).forEach(file => {
+    files.push(file);
+  });
+  let index = readlineSync.keyInSelect(files, 'Load Character by Name: ');
+  let loadedSheet = fs.readFileSync(`./saved/${files[index]}`);
+  return JSON.parse(loadedSheet);
+};
+
+const chooseName = (charSheet) => {
+  charSheet.name = readlineSync.question('Set Character Name: ');
   return charSheet;
 };
 
 const chooseRace = (charSheet) => {
   let answers = ['Human (+1 To All Stats)', 'Elf (+2 Dexterity)', 'Dwarf (+2 Constitution)', 'Half-Orc (+2 Strength)'];
-  let index = readlineSync.keyInSelect(answers, '');
+  let index = readlineSync.keyInSelect(answers, 'Choose Race: ');
   if (index === 0) {
     charSheet.attributes.Str += 1;
     charSheet.attributes.Dex += 1;
@@ -21,12 +59,33 @@ const chooseRace = (charSheet) => {
     charSheet.attributes.Int += 1;
     charSheet.attributes.Wis += 1;
     charSheet.attributes.Cha += 1;
+    charSheet.race = 'Human';
   } else if (index === 1) {
     charSheet.attributes.Dex += 2;
+    charSheet.race = 'Elf';
   } else if (index === 2) {
     charSheet.attributes.Con += 2;
+    charSheet.race = 'Dwarf';
   } else if (index === 3) {
     charSheet.attributes.Str += 2;
+    charSheet.race = 'Half-Orc';
+  }
+  return charSheet;
+};
+
+const chooseAttributes = (charSheet) => {
+  let generatedAttributeList = dice.genAtr();
+  let stats = ['Str', 'Dex', 'Con', 'Int', 'Wis', 'Cha'];
+  let index1 = 0;
+  let index2 = 0;
+  while (generatedAttributeList.length > 0) {
+    clear();
+    design.sheetDesign(charSheet);
+    index1 = readlineSync.keyInSelect(generatedAttributeList, 'Select value to distribute: ');
+    index2 = readlineSync.keyInSelect(stats, 'Add the value to the following attribute: ');
+    charSheet.attributes[stats[index2]] += generatedAttributeList.splice(index1, 1)[0];
+    stats.splice(index2, 1);
+    modifierCalculator(charSheet);
   }
   return charSheet;
 };
@@ -41,7 +100,7 @@ const modifierCalculator = (character) => {
 
 module.exports = {
   mainMenu,
-  newCharacter,
-  chooseRace,
-  modifierCalculator
+  createCharacter,
+  modifierCalculator,
+  loadChars
 };
